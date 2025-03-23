4442830ea355c55f4512ea645e388e31b8740486
ajh-219 <oopp219@naver.com>
2025-03-23 11:47:19 +0900

    2025-03-23
    2025-03-23

---
+++ CPlus/공배수.cpp
@@ -0,0 +1,29 @@
+//#include "Header.h"
+//
+//int main()
+//{
+//	
+//	int last = 0;
+//	int sum = 0;
+//	while (1)
+//	{
+//		for (int i = 0; i < 100; i++)
+//		{
+//			
+//			if (i % 3 == 0 && i % 4 == 0)
+//			{
+//				sum += i;
+//				last = i;
+//			}
+//			
+//		}
+//		if (sum > 500)
+//		{
+//			cout << "3과 4의 공배수 합 : " << sum <<  endl;
+//			cout << "마지막 값 : " << last << endl;
+//			break;
+//		}
+//	}
+//
+//	return 0;
+//}
