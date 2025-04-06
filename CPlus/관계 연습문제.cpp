dddb6f56a0216fce4eee2fa3b623ce8f86fa9641
ajh-219 <oopp219@naver.com>
2025-04-06 11:44:40 +0900

    250406
    250406

---
+++ CPlus/관계 연습문제.cpp
@@ -0,0 +1,38 @@
+//#include "Header.h"
+//
+//// 다음 설명을 통해, 클래스를 설계하세요.
+//// 1. 자동차는 엔진을 가지고 있습니다.
+//class Engine 
+//{
+//
+//};
+//class car
+//{
+//private:
+//	
+//public:
+//	Engine engine;
+//
+//};
+//
+//// 2. 철수는 오늘 옷을 사서 집에 가고 있습니다.
+//class Cloth 
+//{
+//
+//};
+//class 철수
+//{
+//public : 
+//	Cloth cloth;
+//
+//};
+//
+//
+//
+//
+//int main()
+//{
+//
+//
+//	return 0;
+//}
